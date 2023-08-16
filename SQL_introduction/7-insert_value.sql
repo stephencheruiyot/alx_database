d5ba8f3b1021bfcc3189848b6e73bd839ad84d29
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-16 19:13:07 +0300

    add 2 files

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,5 @@
+INSERT INTO hbtn_0c_0.first_table (id, name)
+VALUES (89, 'Holberton School');
+
+--Execute in terminal
+mysql -u <username> -p -e "USE <database_name>; INSERT INTO first_table (id, name) VALUES (89, 'Holberton School');"
