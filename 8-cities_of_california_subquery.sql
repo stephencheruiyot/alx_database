9252e145010f02ed804eabb87f00826acc253f74
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-16 21:17:28 +0300

    add 8 cities

---
+++ 8-cities_of_california_subquery.sql
@@ -0,0 +1,12 @@
+-- Use the specified database
+USE hbtn_0d_usa;
+
+-- Get the ID of California state
+SELECT id INTO @california_id FROM states WHERE name = 'California';
+
+-- Retrieve cities in California and sort them by cities.id
+SELECT cities.id, cities.name
+FROM cities, states
+WHERE cities.state_id = @california_id
+  AND states.id = @california_id
+ORDER BY cities.id ASC;
