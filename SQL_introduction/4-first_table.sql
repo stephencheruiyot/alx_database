3578ee086c8119bb8338d07f5b079aeaaa684746
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-16 20:05:39 +0300

    add first table

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,7 @@
-USE mysql;
+/*script that creates the first_table 
+if it doesn't exist in the specified database:*/
 
-CREATE TABLE IF NOT EXISTS first_table (
-    id INT,
-    name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS `mysql`.`first_table` (
+    `id` INT,
+    `name` VARCHAR(256)
 );
