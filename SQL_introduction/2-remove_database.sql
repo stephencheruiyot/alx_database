08c8a9446f2d7d7a5d167ac2be0ed51cded2da42
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-16 19:49:48 +0300

    add delete database

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,15 +1,3 @@
--- Connect to MySQL server (replace with your connection details)
-USE mysql;
--- Your MySQL username and password
-SET @username = 'root';
-SET @password = 'Folio9470m';
-
 -- Attempt to delete the database
 DROP DATABASE IF EXISTS hbtn_0c_0;
 
--- Display success or error message
-SET @result = IF(@@error_number = 0, 'Database hbtn_0c_0 deleted.', 'Database hbtn_0c_0 does not exist.');
-SELECT @result AS Message;
-
--- Close the connection
-QUIT;
