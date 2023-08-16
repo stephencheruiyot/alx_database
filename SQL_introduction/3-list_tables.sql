8668ffcaec3c688de3f5ba72602201925f3baf02
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-16 19:58:48 +0300

    add list tables

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,7 +1,4 @@
--- Store the passed argument as the database name
-SET @db_name = mysql;
-
 -- Query to list all tables in the specified database
 SELECT table_name
 FROM information_schema.tables
-WHERE table_schema = @db_name;
+WHERE table_schema = mysql;
