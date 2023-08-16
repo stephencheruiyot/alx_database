13f30def349ef5d5e91bbb8073045a55a83f1c64
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-16 20:45:37 +0300

    add privileges

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,15 @@
+-- Connect to the MySQL server
+USE mysql;
+
+-- List privileges for user_0d_1
+SELECT *
+FROM user
+WHERE User IN ('user_0d_1')\G;
+
+-- List privileges for user_0d_2
+SELECT *
+FROM user
+WHERE User IN ('user_0d_2')\G;
+
+-- Disconnect from the MySQL server
+QUIT;
