a8a58f563be83fa0f713db09a2192045e6dcfb62
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-16 20:24:48 +0300

    add 6 list values

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,14 +1,6 @@
-#!/bin/bash
+/* script that lists all rows
+ of the first_table from the
+  hbtn_0c_0 database in your MySQL  */
+USE hbtn_0c_0;
 
-# Replace with your MySQL credentials
-MYSQL_USER="root"
-MYSQL_PASSWORD="Folio9470m"
-MYSQL_HOST="localhost"
-DATABASE_NAME="hbtn_0c_0"
-TABLE_NAME="first_table"
-
-# Construct the MySQL command
-MYSQL_COMMAND="mysql -$MYSQL_USER -p$MYSQL_PASSWORD -h$MYSQL_HOST $DATABASE_NAME -e \"SELECT * FROM $TABLE_NAME;\""
-
-# Execute the command
-eval "$MYSQL_COMMAND"
+SELECT * FROM first_table;
