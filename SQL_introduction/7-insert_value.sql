e886e99de951b42c09c3a8e2a4596c211ceb83d0
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-16 20:19:36 +0300

    add first add

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,5 +1,6 @@
-INSERT INTO hbtn_0c_0.first_table (id, name)
-VALUES (89, 'Holberton School');
+/*script that you can use to insert a new row into 
+the first_table in the hbtn_0c_0 database   */
+INSERT INTO hbtn_0c_0.first_table (id, name) VALUES (89, 'Holberton School');
 
---Execute in terminal
-mysql -u <username> -p -e "USE <database_name>; INSERT INTO first_table (id, name) VALUES (89, 'Holberton School');"
+--run script
+mysql -u your_username -p -h your_host -D hbtn_0c_0 < your_script.sql
