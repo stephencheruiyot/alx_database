49143c70d5147537573c718a6f24917faa4f804c
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-17 21:48:00 +0300

    add force

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,7 +1,5 @@
-USE hbtn_0d_2; -- Replace 'your_database_name' with the actual name of your database
-
--- Create the table if it doesn't already exist
 CREATE TABLE IF NOT EXISTS force_name (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(256) NOT NULL
+    id INT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
 );
