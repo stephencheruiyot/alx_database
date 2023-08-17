172a966f426026e2b0b077484d12de523a67fdbf
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-17 22:35:01 +0300

    modify code

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,5 @@
 -- script that retrieves the full description of the first_table 
 -- without using the DESCRIBE or EXPLAIN 
-SELECT * FROM hbtn_0c_0.first_table;
+SELECT GROUP_CONCAT(column_name, ' ', column_type) AS 'Table Create Table'
+FROM information_schema.columns
+WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table';
