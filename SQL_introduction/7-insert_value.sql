808e13f51f4d368a00071d8525db473a2b48d09a
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-17 22:45:10 +0300

    remove comment

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,6 +1,3 @@
--- script that you can use to insert a new row into 
--- the first_table in the hbtn_0c_0 database   */
+-- script that you can use to insert a new row 
 INSERT INTO hbtn_0c_0.first_table (id, name) VALUES (89, 'Holberton School');
 
--- run script
--- mysql -u your_username -p -h your_host -D hbtn_0c_0 < your_script.sql
