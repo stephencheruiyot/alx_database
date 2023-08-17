06c2671a551b6341fc72ae7302f8180dd412278f
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-17 22:50:20 +0300

    add 5full

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,11 +1,5 @@
 -- script that retrieves the full description of the first_table 
--- without using the DESCRIBE or EXPLAIN 
--- Set the database name passed as an argument to the mysql command
-SET @dbName = 'hbtn_0c_0';
-
--- Build the SQL query to retrieve table information
-SET @sqlQuery = CONCAT('SELECT GROUP_CONCAT(\'`\', column_name, \'`\', column_type, IF(IS_NULLABLE = \'NO\', \'NOT NULL\', \'\'), IF(column_default IS NOT NULL, CONCAT(\'DEFAULT \', column_default), \'\')) AS table_description
-FROM information_schema.columns
-WHERE table_schema = \'', @dbName, '\' AND table_name = \'first_table\'
-GROUP BY table_name');
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'first_table';
 
