4b981d564ee0b75039f8121ab865015043b12cb9
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-17 22:46:59 +0300

    delete db

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- script that you can use to insert a new row 
-INSERT INTO hbtn_0c_0.first_table (id, name) VALUES (89, 'Holberton School');
+INSERT INTO first_table (id, name) VALUES (89, 'Holberton School');
 
