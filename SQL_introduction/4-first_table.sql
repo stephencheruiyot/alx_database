075cef4b33751a029416747202a4b29920ec4258
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-17 18:16:05 +0300

    create table if

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 -- script that creates the first_table 
--- if it doesn't exist in the specified database:*/
+-- if it doesn't exist in the specified database:
 
 CREATE TABLE IF NOT EXISTS `hbtn_0c_0`.`first_table`
  (
