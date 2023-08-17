2939370026afa43dfed6ff595e188b3292a8c2b1
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-17 22:17:04 +0300

    delete state

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Retrieve cities' information along with their corresponding state names
-SELECT cities.id, cities.name, states.name AS state_name
+SELECT cities.id, cities.name, states.name AS name
 FROM cities
 JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
