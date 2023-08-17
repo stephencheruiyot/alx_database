d15ceec1cd099ecec0d0e485109f878a6e364439
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-17 21:42:05 +0300

    modify code

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -2,15 +2,6 @@
 CREATE USER 'user_0d_1'@'localhost';
 CREATE USER 'user_0d_2'@'localhost';
 
--- Grant privileges to user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
--- Grant privileges to user_0d_2
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
-
--- Flush privileges to apply changes
-FLUSH PRIVILEGES;
-
 -- Show privileges for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
