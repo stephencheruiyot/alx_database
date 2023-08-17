5a9b94d1ddd1c1a7b311e2088a1cfe32417ba5af
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-17 22:24:24 +0300

    remove comnts

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,10 +1,8 @@
 -- script that retrieves the full description of the first_table 
--- without using the DESCRIBE or EXPLAIN */
+-- without using the DESCRIBE or EXPLAIN 
 SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, COLUMN_DEFAULT, IS_NULLABLE
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'hbtn_0c_0'
 AND TABLE_NAME = 'first_table';
 
 
--- run code AS
--- mysql -u username -p -e "USE hbtn_0c_0; SELECT * FROM first_table;"
