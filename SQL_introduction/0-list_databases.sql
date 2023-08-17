c57ac72fe594e8083030bcb1f4c43559246e682e
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-17 17:23:51 +0300

    add show databases

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,11 +1,3 @@
 -- a script that lists all databases of your MySQL server.
--- Connect to the MySQL server
-
-mysql -u root -p Folio9470m
-
 -- List all databases
 SHOW DATABASES;
-
--- Exit the MySQL client
-EXIT;
-
