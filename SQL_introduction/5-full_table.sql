423f1336c79701c270f7a004a6338f8e0820a977
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-17 18:38:29 +0300

    add comments

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,5 @@
-/*script that retrieves the full description of the first_table 
-without using the DESCRIBE or EXPLAIN */
+-- script that retrieves the full description of the first_table 
+-- without using the DESCRIBE or EXPLAIN */
 SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, COLUMN_DEFAULT, IS_NULLABLE
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'hbtn_0c_0'
