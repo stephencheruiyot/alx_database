1c073a3a556c74afea0d6a568375ed384c464ff0
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-17 22:15:05 +0300

    remove use

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,3 @@
--- Connect to the specified database
-USE hbtn_0d_usa;
-
 -- Retrieve cities' information along with their corresponding state names
 SELECT cities.id, cities.name, states.name AS state_name
 FROM cities
