b99158c123687689eb9f998b7dee4b6c69c18d70
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-17 22:20:35 +0300

    remove db

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,7 +1,7 @@
 -- script that creates the first_table 
 -- if it doesn't exist in the specified database:
 
-CREATE TABLE IF NOT EXISTS `hbtn_0c_0`.`first_table`
+CREATE TABLE IF NOT EXISTS `first_table`
  (
     `id` INT,
     `name` VARCHAR(256)
